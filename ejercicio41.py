c3f811031276d9be734d3bea4809d21f55cf722b
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-10 10:02:41 -0500

    Add files via upload

---
+++ ejercicio41.py
@@ -0,0 +1,3 @@
+print ( "Los diez primeros números naturales son:" )
+para  x  en el  rango ( 0 , 10 ):   # algunos cuentan los numeros naturales desde 0 y otros desde 1
+    print ( x , "" , end = "" )
